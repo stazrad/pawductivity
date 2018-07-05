a46b16336df3a7377db4de53ba07b51ecf8be5b7
Jake Webb <jakewebbsite@gmail.com>
2018-07-05 17:33:30 -0500

    push notifications working!

--- src/components/App.js
+++ src/components/App.js
@@ -8,45 +8,6 @@ import TimeSlider from 'components/TimeSlider'
 import Timer from 'components/Timer'
 console.log('push', PushNotification)
 
-PushNotification.configure({
-
-    // (optional) Called when Token is generated (iOS and Android)
-    onRegister: function(token) {
-        console.log( 'TOKEN:', token );
-    },
-
-    // (required) Called when a remote or local notification is opened or received
-    onNotification: function(notification) {
-        console.log( 'NOTIFICATION:', notification );
-
-        // process the notification
-
-        // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
-        notification.finish(PushNotificationIOS.FetchResult.NoData);
-    },
-
-    // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications, but is need to receive remote push notifications)
-    senderID: "YOUR GCM SENDER ID",
-
-    // IOS ONLY (optional): default: all - Permissions to register.
-    permissions: {
-        alert: true,
-        badge: true,
-        sound: true
-    },
-
-    // Should the initial notification be popped automatically
-    // default: true
-    popInitialNotification: true,
-
-    /**
-      * (optional) default: true
-      * - Specified if permissions (ios) and token (android and ios) will requested or not,
-      * - if not, you must call PushNotificationsHandler.requestPermissions() later
-      */
-    requestPermissions: true,
-})
-
 export default class App extends React.Component {
     constructor () {
         super()
@@ -74,6 +35,37 @@ export default class App extends React.Component {
 
     componentDidMount () {
         AppState.addEventListener('change', this.handleAppStateChange)
+
+        PushNotification.configure({
+            // (optional) Called when Token is generated (iOS and Android)
+            onRegister: function(token) {
+                console.log( 'TOKEN:', token );
+            },
+            // (required) Called when a remote or local notification is opened or received
+            onNotification: function(notification) {
+                console.log( 'NOTIFICATION:', notification );
+                // process the notification
+                // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+                notification.finish(PushNotificationIOS.FetchResult.NoData);
+            },
+            // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications, but is need to receive remote push notifications)
+            senderID: "YOUR GCM SENDER ID",
+            // IOS ONLY (optional): default: all - Permissions to register.
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true
+            },
+            // Should the initial notification be popped automatically
+            // default: true
+            popInitialNotification: true,
+            /**
+              * (optional) default: true
+              * - Specified if permissions (ios) and token (android and ios) will requested or not,
+              * - if not, you must call PushNotificationsHandler.requestPermissions() later
+              */
+            requestPermissions: true,
+        })
     }
 
     componentWillUnMount () {
@@ -83,7 +75,10 @@ export default class App extends React.Component {
     handleAppStateChange (appState) {
         if (appState === 'background') {
             // TODO add push notification here
-            console.log('UH OH, APP CLOSED!')
+            const details = {
+                alertBody: 'You left the app, bitch!'
+            }
+            PushNotification.presentLocalNotification(details)
         }
     }
 
