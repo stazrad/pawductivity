a4d2bbcd014fa81551b8e96c77eabb327f0dd96f
Jake Webb <jakewebbsite@gmail.com>
2018-07-05 16:27:53 -0500

    add listener for when app is closed

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,51 @@
 // packages
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import PushNotification from 'react-native-push-notification'
+import { AppState, StyleSheet, Text, View } from 'react-native'
+// import PushNotification from 'react-native-push-notification'
 
 // imports
 import TimeSlider from 'components/TimeSlider'
-console.log('push', PushNotification)
-
-PushNotification.configure({
-
-    // (optional) Called when Token is generated (iOS and Android)
-    onRegister: function(token) {
-        console.log( 'TOKEN:', token );
-    },
-
-    // (required) Called when a remote or local notification is opened or received
-    onNotification: function(notification) {
-        console.log( 'NOTIFICATION:', notification );
-
-        // process the notification
-
-        // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
-        notification.finish(PushNotificationIOS.FetchResult.NoData);
-    },
-
-    // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications, but is need to receive remote push notifications)
-    senderID: "YOUR GCM SENDER ID",
-
-    // IOS ONLY (optional): default: all - Permissions to register.
-    permissions: {
-        alert: true,
-        badge: true,
-        sound: true
-    },
-
-    // Should the initial notification be popped automatically
-    // default: true
-    popInitialNotification: true,
-
-    /**
-      * (optional) default: true
-      * - Specified if permissions (ios) and token (android and ios) will requested or not,
-      * - if not, you must call PushNotificationsHandler.requestPermissions() later
-      */
-    requestPermissions: true,
-})
+import Timer from 'components/Timer'
+// console.log('push', PushNotification)
+//
+// PushNotification.configure({
+//
+//     // (optional) Called when Token is generated (iOS and Android)
+//     onRegister: function(token) {
+//         console.log( 'TOKEN:', token );
+//     },
+//
+//     // (required) Called when a remote or local notification is opened or received
+//     onNotification: function(notification) {
+//         console.log( 'NOTIFICATION:', notification );
+//
+//         // process the notification
+//
+//         // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+//         notification.finish(PushNotificationIOS.FetchResult.NoData);
+//     },
+//
+//     // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications, but is need to receive remote push notifications)
+//     senderID: "YOUR GCM SENDER ID",
+//
+//     // IOS ONLY (optional): default: all - Permissions to register.
+//     permissions: {
+//         alert: true,
+//         badge: true,
+//         sound: true
+//     },
+//
+//     // Should the initial notification be popped automatically
+//     // default: true
+//     popInitialNotification: true,
+//
+//     /**
+//       * (optional) default: true
+//       * - Specified if permissions (ios) and token (android and ios) will requested or not,
+//       * - if not, you must call PushNotificationsHandler.requestPermissions() later
+//       */
+//     requestPermissions: true,
+// })
 
 export default class App extends React.Component {
     constructor () {
@@ -72,7 +73,18 @@ export default class App extends React.Component {
     }
 
     componentDidMount () {
-        console.log()
+        AppState.addEventListener('change', this.handleAppStateChange)
+    }
+
+    componentWillUnMount () {
+        AppState.removeEventListener('change', this.handleAppStateChange)
+    }
+
+    handleAppStateChange (appState) {
+        if (appState === 'background') {
+            // TODO add push notification here
+            console.log('UH OH, APP CLOSED!')
+        }
     }
 
     render () {
